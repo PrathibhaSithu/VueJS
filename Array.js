caebecac4039560cad891d6ff699e0c84ce7fa24
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2024-08-05 13:15:00 +0530

    update

--- Array.js
+++ Array.js
@@ -3,19 +3,21 @@ const app = Vue.createApp({
         return{
             showBooks: true,
             books: [
-                {title: 'Name of the Wind', author: 'Patrick Rothfuss'},
-                {title: 'The way of kings', author: 'Brandon sanderson'},
-                {title: 'The final empire', author: 'Brandon sanderson'},
-                {title: 'Sourceres Stone', author: 'JK Rowling'},
-                {title: 'Half blood prince', author: 'JK Rowling'},
+                { title: 'Name of the Wind', author: 'Patrick Rothfuss' },
+                { title: 'The way of kings', author: 'Brandon sanderson' },
+                { title: 'The final empire', author: 'Brandon sanderson' },
+                { title: 'Sourceres Stone', author: 'JK Rowling' },
+                { title: 'Half blood prince', author: 'JK Rowling' },
             ],
+            url: 'https://github.com/',
         }
     },
+
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks
-    },
-}
+        },
+    }
 })
 
 app.mount('#app')
