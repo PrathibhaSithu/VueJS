90a28894062fb6aadbe5af473b7ef7b202129b3b
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2024-08-06 09:51:44 +0530

    update

--- dynamic.js
+++ dynamic.js
@@ -16,6 +16,9 @@ const app = Vue.createApp({
         toggleShowBooks(){
             this.showBooks = !this.showBooks
         },
+        toggleFav(book){
+            book.isFav = !book.isFav
+        },
     }
 })
 
